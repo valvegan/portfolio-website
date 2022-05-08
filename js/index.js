b19b59666b6b1c977270b468eb84f32c46b07490
valvegan <valentinavaccari@hotmail.com>
2022-05-09 00:40:09 +0100

    js start

--- js/index.js
+++ js/index.js
@@ -1,6 +1,75 @@
-var images = document.images,
-    data = [];
-for (var i = 0, len = images.length; i < len; i++) {
-  data.push(images[i].src);
-  console.log(data[i]);
-}
+//fetch json data about projects
+getDataTrial = () => {
+  fetch("js/projects.json")
+    .then((response) => response.json())
+    .then((data) =>
+      data.forEach((item) => {
+        //convert pokemon names to uppercase
+        let title = item.Title;
+        let description = item.Description;
+        console.log(title);
+      })
+    )
+    .catch((e) => {
+      console.log(e);
+    });
+};
+getDataTrial();
+
+let portfolioProjects = (function () {
+  let projects = [];
+
+  function loadList() {
+    return fetch("projects.json")
+      .then((response) => response.json())
+      .then(
+        (data) => console.log(data),
+        data.forEach((item) => {
+          console.log(item);
+          //convert pokemon names to uppercase
+          let title = item.Title;
+          let description = item.description;
+          add(project);
+        })
+      )
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  function displayWork(project) {
+    let container = document.querySelector("#work");
+    let listWork = document.createElement("li");
+    let button = document.createElement("button");
+    let title = document.createElement("h4");
+    listWork.classList.add("list-item");
+    button.classList.add("grid__item");
+    button.style.backgroundImage = "url(" + project.img + ")";
+    title.innerText = project.name;
+    listWork.appendChild(title);
+    listWork.appendChild(button);
+    container.appendChild(listWork);
+    button.addEventListener("click", () => {
+      displayDetails(project);
+      toggleModal();
+    });
+  }
+
+  function getAll() {
+    return projects;
+  }
+
+  return {
+    loadList: loadList,
+    displayWork: displayWork,
+    getAll: getAll,
+  };
+})();
+
+portfolioProjects
+  .getAll()
+
+  .forEach((project) => {
+    portfolioProjects.loadList(project);
+    console.log(project);
+  });
